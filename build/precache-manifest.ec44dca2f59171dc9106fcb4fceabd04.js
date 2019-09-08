0ae1f67d246c52b95e5a69534be02f26cb2244c9
Fidel Seehawer <fidel@kreativgebiet.com>
2019-09-08 13:48:06 +0200

    Add files

---
+++ build/precache-manifest.ec44dca2f59171dc9106fcb4fceabd04.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07157a4fb8b5157896c32e099c1bbbbb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "899b5c0826392c8ff30d",
+    "url": "./static/css/main.99310c17.chunk.css"
+  },
+  {
+    "revision": "2281fb7b5ee49bc15fed",
+    "url": "./static/js/2.9570b62d.chunk.js"
+  },
+  {
+    "revision": "899b5c0826392c8ff30d",
+    "url": "./static/js/main.c4f1b844.chunk.js"
+  },
+  {
+    "revision": "25d995a907bf62b4b3df",
+    "url": "./static/js/runtime~main.9affbe4a.js"
+  },
+  {
+    "revision": "cbd07d0a684e906d4869e99c55b634b5",
+    "url": "./static/media/1.cbd07d0a.jpg"
+  },
+  {
+    "revision": "d0a0fcbe150ef017689c56cb34570fed",
+    "url": "./static/media/2.d0a0fcbe.jpg"
+  },
+  {
+    "revision": "be788f2b2a88e9e3e5579cbb266df53c",
+    "url": "./static/media/3.be788f2b.jpg"
+  },
+  {
+    "revision": "447a05e0898cdbdadbc6504ae27cf06c",
+    "url": "./static/media/4.447a05e0.jpg"
+  },
+  {
+    "revision": "f37f0ae83dc98569a226e5ed17839234",
+    "url": "./static/media/5.f37f0ae8.jpg"
+  },
+  {
+    "revision": "f6f47f6caba25e7343a7abc795f37d8c",
+    "url": "./static/media/6.f6f47f6c.jpg"
+  },
+  {
+    "revision": "1c66dddb4803c126fb849cfe77f5443f",
+    "url": "./static/media/7.1c66dddb.jpg"
+  },
+  {
+    "revision": "af2376a6eb07c0fca6e74a0193ab48da",
+    "url": "./static/media/8.af2376a6.jpg"
+  }
+]);
